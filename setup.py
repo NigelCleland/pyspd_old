00298e4045024359dc401889773b53c8f68f6021
NigelCleland <nigel.cleland@gmail.com>
2013-03-28 09:25:40 +1300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="pyspd",
+      version='0.1',
+      description="Simplified SPD model for research",
+      author="Nigel Cleland",
+      author_email="nigel.cleland@gmail.com",
+      packages=['pyspd'],
+    )
